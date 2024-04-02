3567035093410687e407811c0e0f2b676455d126
Mevin5611 <mevinmevi88@gmail.com>
2024-04-02 12:20:02 +0530

    first commit

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const cors = require('cors')
+const express = require("express");
+const app = express();
 
-app.use(cors())
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+module.exports = app;
